42d36241b67d0fcff0bcfd75c7ec0dc849e5b6fd
petrolingus <petrolingus@gmail.com>
2023-04-29 23:05:12 +0300

    feat(process-service): add thread pool

--- process-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/processservice/ImageService.java
+++ process-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/processservice/ImageService.java
@@ -1,5 +1,6 @@
 package me.petrolingus.mandelbrotsetvisualization.processservice;
 
+import ch.qos.logback.core.util.TimeUtil;
 import jakarta.annotation.PostConstruct;
 import me.petrolingus.mandelbrotsetvisualization.dao.CompletedTask;
 import me.petrolingus.mandelbrotsetvisualization.dao.Task;
@@ -14,11 +15,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.*;
 
 @Service
 @EnableScheduling
@@ -34,6 +36,8 @@ public class ImageService {
 
     final RestTemplate restTemplate;
 
+    ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
     public ImageService(Mandelbrot mandelbrot, RestTemplate restTemplate) {
         this.mandelbrot = mandelbrot;
         this.restTemplate = restTemplate;
@@ -48,31 +52,36 @@ public class ImageService {
     @Scheduled(initialDelay = 0, fixedDelay = 1)
     public void process() {
 
-        String url = "http://localhost:8080/tasks";
+        for (int i = 0; i < executor.getMaximumPoolSize() - executor.getActiveCount(); i++) {
+            executor.submit(() -> {
 
-        // Check available tasks and get it if exists
-        Task task = restTemplate.getForObject(url, Task.class);
+                String url = "http://localhost:8080/tasks";
 
-        if (task == null) {
-            return;
+                // Check available tasks and get it if exists
+                Task task = restTemplate.getForObject(url, Task.class);
+
+                if (task == null) {
+                    return;
+                }
+
+                LOGGER.info(task.toString());
+
+                // Make task
+                long start = System.currentTimeMillis();
+                int[] mandelbrotImage = mandelbrot.getMandelbrotImage(task.size(), task.xc(), task.yc(), task.scale(), task.iterations(), hue, saturation);
+                long stop = System.currentTimeMillis();
+
+                LOGGER.info("Task {} completed in {} ms", task.uuid().toString(), (stop - start));
+
+                // Create the request body by wrapping the object in HttpEntity
+                CompletedTask completedTask = new CompletedTask(task.uuid(), mandelbrotImage, task.x(), task.y(), task.size());
+                HttpEntity<CompletedTask> request = new HttpEntity<>(completedTask);
+
+                // Send the request body in HttpEntity for HTTP POST request
+                restTemplate.postForObject(url, request, String.class);
+
+            });
         }
-
-        LOGGER.info(task.toString());
-
-        // Make task
-        long start = System.currentTimeMillis();
-        int[] mandelbrotImage = mandelbrot.getMandelbrotImage(task.size(), task.xc(), task.yc(), task.scale(), task.iterations(), hue, saturation);
-        long stop = System.currentTimeMillis();
-
-        LOGGER.info("Task {} completed in {} ms", task.uuid().toString(), (stop - start));
-
-        // Create the request body by wrapping the object in HttpEntity
-        CompletedTask completedTask = new CompletedTask(task.uuid(), mandelbrotImage, task.x(), task.y(), task.size());
-        HttpEntity<CompletedTask> request = new HttpEntity<>(completedTask);
-
-        // Send the request body in HttpEntity for HTTP POST request
-        String taskCreateResponse = restTemplate.postForObject(url, request, String.class);
-
-//        LOGGER.info(taskCreateResponse);
     }
+
 }
