d189ae88433701752766c34acd3b2f84c5ea0d00
petrolingus <petrolingus@gmail.com>
2023-05-03 00:01:35 +0300

    fix(ui-service): remove duplicated annotation

--- ui-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/uiservice/UiController.java
+++ ui-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/uiservice/UiController.java
@@ -22,7 +22,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@@RestController
+@RestController
 public class UiController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UiController.class);
