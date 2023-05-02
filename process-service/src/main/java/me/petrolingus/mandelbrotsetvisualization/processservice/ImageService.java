1d17520cd32bb2051c63e29bc578324ef7bd5290
petrolingus <petrolingus@gmail.com>
2023-05-02 23:55:16 +0300

    refactor(process-service): just code refactor

--- process-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/processservice/ImageService.java
+++ process-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/processservice/ImageService.java
@@ -1,37 +1,33 @@
 package me.petrolingus.mandelbrotsetvisualization.processservice;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ThreadLocalRandom;
 
-@RestController("/")
+@RestController
 public class ImageService {
 
     private final Mandelbrot mandelbrot;
 
+    private final float hue;
+
+    private final float saturation;
+
     public ImageService(Mandelbrot mandelbrot) {
         this.mandelbrot = mandelbrot;
+        this.hue = ThreadLocalRandom.current().nextFloat();
+        this.saturation = 0.2f * ThreadLocalRandom.current().nextFloat() + 0.2f;
     }
 
-    private float hue;
-
-    private float saturation;
-
-    @PostConstruct
-    public void init() {
-        hue = ThreadLocalRandom.current().nextFloat();
-        saturation = 0.2f * ThreadLocalRandom.current().nextFloat() + 0.2f;
-    }
-
-    @GetMapping("mandelbrot")
-    public int[] getMandelbrot(@RequestParam int size,
-                               @RequestParam double xc,
-                               @RequestParam double yc,
-                               @RequestParam double scale,
-                               @RequestParam int maxIterations
+    @GetMapping("/api/v1/generate-mandelbrot-tile")
+    public int[] generateMandelbrotTile(@RequestParam int size,
+                                        @RequestParam double xc,
+                                        @RequestParam double yc,
+                                        @RequestParam double scale,
+                                        @RequestParam int maxIterations
     ) {
         return mandelbrot.getMandelbrotImage(size, xc, yc, scale, maxIterations, hue, saturation);
     }
