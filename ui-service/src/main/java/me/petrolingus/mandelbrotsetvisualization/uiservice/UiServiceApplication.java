825bff18c7ab8b9da3c5f983e032c3d09d39e070
petrolingus <petrolingus@gmail.com>
2023-05-02 18:19:25 +0300

    refactor(ui-service): move rest template

--- ui-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/uiservice/UiServiceApplication.java
+++ ui-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/uiservice/UiServiceApplication.java
@@ -2,6 +2,8 @@ package me.petrolingus.mandelbrotsetvisualization.uiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UiServiceApplication {
@@ -10,4 +12,8 @@ public class UiServiceApplication {
 		SpringApplication.run(UiServiceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 }
