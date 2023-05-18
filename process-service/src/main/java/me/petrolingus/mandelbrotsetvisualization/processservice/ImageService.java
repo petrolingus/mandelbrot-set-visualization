840d04b72f5c7694f15042d32218de1e042e77a6
petrolingus <petrolingus@gmail.com>
2023-05-18 04:49:16 +0300

    refactor(process-service): delete unused imports

--- process-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/processservice/ImageService.java
+++ process-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/processservice/ImageService.java
@@ -1,9 +1,8 @@
 package me.petrolingus.mandelbrotsetvisualization.processservice;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicInteger;
