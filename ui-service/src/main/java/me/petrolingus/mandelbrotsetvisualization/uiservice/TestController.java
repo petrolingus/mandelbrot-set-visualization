9a3d205820a043eb3726d55d4a68b0370918a42b
petrolingus <petrolingus@gmail.com>
2023-05-24 07:26:51 +0300

    fix(ui-service): change test controller

--- ui-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/uiservice/TestController.java
+++ ui-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/uiservice/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,7 +17,7 @@ public class TestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
-    private static final int MAX_NUMBER_OF_MEASURES = 32;
+    private static final int MAX_NUMBER_OF_MEASURES = 33;
 
     final RestTemplate restTemplate;
 
@@ -30,6 +31,8 @@ public class TestController {
     @PostMapping("/api/v1/launch-test")
     public void launchTest() throws IOException, InterruptedException {
 
+        LOGGER.info("Start testing...");
+
         UUID uuid = UUID.randomUUID();
 
         int size = 1024;
@@ -38,8 +41,10 @@ public class TestController {
         double scale = 2.0;
         int iterations = 512;
 
+        List<Double> meanList = new ArrayList<>();
+
         // Subdivision can not more than log(size) / log(2)
-        for (int subdivision = 0; subdivision < 6; subdivision++) {
+        for (int subdivision = 0; subdivision < 7; subdivision++) {
 
             List<Long> measures = new ArrayList<>();
             for (int i = 0; i < MAX_NUMBER_OF_MEASURES; i++) {
@@ -50,10 +55,13 @@ public class TestController {
             }
 
             double avg = measures.stream().mapToDouble(Long::doubleValue).average().orElse(-1);
-            double mean = measures.stream().sorted().toList().get(16);
+            double mean = measures.stream().sorted().toList().get(17);
+            meanList.add(mean);
 
             LOGGER.info("Test {}# subdivisions: {}, avg: {}ms, mean: {}ms", uuid, subdivision, avg, mean);
         }
+
+        LOGGER.info("Result of {} test: {}", uuid, Arrays.toString(meanList.toArray()));
     }
 
 }
