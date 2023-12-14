dfe8f081ee938d81122198cd5e463a9928714aa9
petrolingus <petrolingus@gmail.com>
2023-12-15 02:31:33 +0300

    fix

--- process-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/processservice/ImageService.java
+++ process-service/src/main/java/me/petrolingus/mandelbrotsetvisualization/processservice/ImageService.java
@@ -16,7 +16,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.time.temporal.ChronoUnit;
 import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
