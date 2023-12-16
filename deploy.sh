765204ab236e7a74b34362880ad1b2279bd4aeea
petrolingus <petrolingus@gmail.com>
2023-12-17 01:29:17 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ kubectl apply -f process-service/kubernetes/service.yaml -n mandelbrot
 kubectl apply -f process-service/kubernetes/ingress.yaml -n mandelbrot
 
 kubectl apply -f ui-service/kubernetes/ui-service-service.yaml -n mandelbrot
-kubectl apply -f ui-service/kubernetes/ui-service-ingress.yaml -n mandelbrot
+#kubectl apply -f ui-service/kubernetes/ui-service-ingress.yaml -n mandelbrot
 kubectl expose deployment/ui-service-service --type=LoadBalancer --port 8180 --target-port 8080 --name=ui-service-service -n mandelbrot
-kubectl expose deployment/ui-service-ingress --type=LoadBalancer --port 8280 --target-port 8080 --name=ui-service-ingress -n mandelbrot
+#kubectl expose deployment/ui-service-ingress --type=LoadBalancer --port 8280 --target-port 8080 --name=ui-service-ingress -n mandelbrot
